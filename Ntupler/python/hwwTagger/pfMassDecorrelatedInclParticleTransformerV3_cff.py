bc511aa75bc5a44aa8b29d188a11808c63d6dd2c
Congqiao Li <congqiao.li@cern.ch>
2024-10-28 13:00:56 +0100

    Update v3 model

--- Ntupler/python/hwwTagger/pfMassDecorrelatedInclParticleTransformerV3_cff.py
+++ Ntupler/python/hwwTagger/pfMassDecorrelatedInclParticleTransformerV3_cff.py
@@ -7,13 +7,14 @@ from RecoBTag.ONNXRuntime.boostedJetONNXJetTagsProducer_cfi import boostedJetONN
 # downloaded from https://coli.web.cern.ch/coli/tmp/.230626-003937_partv2_model
 # V03pre1: "ak8_MD_inclv8_part_splitreg_extmass_rmhbs_manual.useamp.large_fc2048.gm5.ddp-bs384-lr6e-4.nepoch100.v3fixsplit2", no H->bs class, split-class regression, no pixel inputs
 # V03beta1: "ak8_MD_inclv8std_rmhbs_manual.useamp.large_fc2048.pemb64_block10.gm5.ddp-bs512-lr7e-4.nepoch100", no H->bs class, entire model (313+313+28 nodes), selected output (24) + hidden neurons
+# V03 (beta4p1): "ak8_MD_inclv10beta4_ul_manual.nlayer10.vispart_as_resid.ddp4-bs640-lr1p2e-3.nepoch100.farm221", entire model (374+2+374+28 nodes), selected output (22) + hidden neurons
 
 pfMassDecorrelatedInclParticleTransformerV3HidLayerJetTags = boostedJetONNXJetTagsProducer.clone(
     src = 'pfMassDecorrelatedInclParticleTransformerV2TagInfos', # v3 still uses v2 tag infos
-    preprocess_json = 'DeepNTuples/Ntupler/data/InclParticleTransformer-MD/ak8/V03beta1/preprocess.json',
-    model_path = 'DeepNTuples/Ntupler/data/InclParticleTransformer-MD/ak8/V03beta1/model.onnx',
+    preprocess_json = 'DeepNTuples/Ntupler/data/InclParticleTransformer-MD/ak8/V03/preprocess.json',
+    model_path = 'DeepNTuples/Ntupler/data/InclParticleTransformer-MD/ak8/V03/model.onnx',
     flav_names = [
-        'probXbb', 'probXcc', 'probXcs', 'probXqq', 'probXtauhtaue', 'probXtauhtaum', 'probXtauhtauh', 'probXWW4q', 'probXWW3q', 'probXWWqqev', 'probXWWqqmv', 'probTopbWqq', 'probTopbWq', 'probTopbWev', 'probTopbWmv', 'probTopbWtauhv', 'probQCD', 'massCorrX2p', 'massCorrXWW', 'massCorrTopbW', 'massCorrQCD', 'probWithMassTopvsQCD', 'probWithMassWvsQCD', 'probWithMassZvsQCD'
+        'probXbb', 'probXcc', 'probXcs', 'probXqq', 'probXtauhtaue', 'probXtauhtaum', 'probXtauhtauh', 'probXWW4q', 'probXWW3q', 'probXWWqqev', 'probXWWqqmv', 'probTopbWqq', 'probTopbWq', 'probTopbWev', 'probTopbWmv', 'probTopbWtauhv', 'probQCD', 'massCorrX2p', 'massCorrGeneric', 'probWithMassTopvsQCD', 'probWithMassWvsQCD', 'probWithMassZvsQCD'
     ] + ['hidNeuron' + str(i).zfill(3) for i in range(256)],
     debugMode = False,
 )
